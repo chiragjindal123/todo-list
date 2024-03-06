9e8a811fd90713edb20c4237ce9d0d12b39a6627
chiragjindal123 <golujindal76@gmail.com>
2024-03-06 19:18:41 +0530

    todo=list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Todo from './todo';
 
 function App() {
+  const [inputList, setInputList] = useState("");
+  const [items, setItems] = useState([]);
+
+  const itemEvent = (event) => {
+    setInputList(event.target.value);
+  };
+
+  const listofitems = () => {
+    setItems((oldItems) => {
+      return [...oldItems, inputList];
+    });
+    setInputList("");
+  };
+
+  const deleteitem = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter((arrEle, index) => {
+        return index !== id;
+      });
+    });
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      listofitems();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Add a Items"
+            value={inputList}
+            onChange={itemEvent}
+            onKeyPress={handleKeyPress}
+          />
+          <button onClick={listofitems}>+</button>
+          <ol>
+            {items.map((itemval, index) => {
+              return (
+                <Todo key={index} id={index} text={itemval} onSelect={deleteitem} />
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
